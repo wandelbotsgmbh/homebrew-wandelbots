ce6b902e4217d57970d61292de551ecfaf2ab9d3
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-10-25 07:38:58 +0000

    Merge branch 'feature/readme' into 'main'
    update readme for homebrew install
    
    See merge request backend/wos-cli!74
    
    -----------------
    
    update cli formula commit 313970a6 / version 0.0.124

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.123"
+  version "0.0.124"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.123/wabocli_macos_amd64-0.0.123.tar.gz"
-      sha256 "88c80f732ed86f00c71ee5193b62f76743c2116db61cb26a15172f3bc13a4af2"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.124/wabocli_macos_amd64-0.0.124.tar.gz"
+      sha256 "2a2d4aafcd4ed93edb4fc1adf9fb63decb15657eedf5f59f29262eafd38128ca"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.123/wabocli_macos_arm64-0.0.123.tar.gz"
-      sha256 "86e0c9b23fcf6aad6f09f6d24b483885bc2822aa4475ae844bf06b67baea3d29"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.124/wabocli_macos_arm64-0.0.124.tar.gz"
+      sha256 "e6c1220db51093163e3470bb84c7540a2bc8d291b24274a6ce18d22db42ed2e3"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.123/wabocli_linux_amd64-0.0.123.tar.gz"
-      sha256 "703079977a8c2ac6fe99776af06ee450ed8a379b4661847fcab67d6852de11a9"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.124/wabocli_linux_amd64-0.0.124.tar.gz"
+      sha256 "a3deac3fb1aa76b5671421084c92fdb8d93e56d9d11ee0a90dab5c40ed184cae"
     end
   end
 
