c6aed4b9924be0614132d20e913a6c2f87ea5376
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-10-30 14:21:40 +0000

    Merge branch 'feature/catalog-poc' into 'main'
    Feature/catalog poc
    
    See merge request backend/wos-cli!76
    
    -----------------
    
    update cli formula commit a5e5875c / version 0.0.125

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.124"
+  version "0.0.125"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.124/wabocli_macos_amd64-0.0.124.tar.gz"
-      sha256 "2a2d4aafcd4ed93edb4fc1adf9fb63decb15657eedf5f59f29262eafd38128ca"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.125/wabocli_macos_amd64-0.0.125.tar.gz"
+      sha256 "91dab33e6d9a5093f0180fb10b6fd735235a0d483af863a3e70e18c25e7d92b6"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.124/wabocli_macos_arm64-0.0.124.tar.gz"
-      sha256 "e6c1220db51093163e3470bb84c7540a2bc8d291b24274a6ce18d22db42ed2e3"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.125/wabocli_macos_arm64-0.0.125.tar.gz"
+      sha256 "0d8bcd966d3a9e4e4a5a4949bb748f85c6501e2ff7bd5d77ec1aefb75f170150"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.124/wabocli_linux_amd64-0.0.124.tar.gz"
-      sha256 "a3deac3fb1aa76b5671421084c92fdb8d93e56d9d11ee0a90dab5c40ed184cae"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.125/wabocli_linux_amd64-0.0.125.tar.gz"
+      sha256 "4788d2071ac3c4188dc8bbf2c7045feeabfcb7ff2557b419b8b85b24a4584c11"
     end
   end
 
