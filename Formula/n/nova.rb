34f46ac4e187a9a9f8be9f3dbf5dc457b7293775
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-11-08 11:16:10 +0000

    Merge branch 'feature/next-auth' into 'main'
    add token to local env
    
    See merge request backend/wos-cli!78
    
    -----------------
    
    update cli formula commit 4f59aa42 / version 0.0.127

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.126"
+  version "0.0.127"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.126/wabocli_macos_amd64-0.0.126.tar.gz"
-      sha256 "294131848c67aa6dcea4be460a24033a5f684ae8757e7da575e4f455ad079fd4"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.127/wabocli_macos_amd64-0.0.127.tar.gz"
+      sha256 "1c724b6f97dbf465784e1ffafe6fe9e92ac0fb67819c12b652f09ab776023ea3"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.126/wabocli_macos_arm64-0.0.126.tar.gz"
-      sha256 "ca2afc1e019d76e1653d23661e3c57e6b95f64213d1b7c47c04195c5322d0fa8"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.127/wabocli_macos_arm64-0.0.127.tar.gz"
+      sha256 "0f586518847546e6377baf690224a4458b58ffebcafca4154be5e4f3525c0cab"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.126/wabocli_linux_amd64-0.0.126.tar.gz"
-      sha256 "9a09b502b5ba2d6625fe47bce894b2e7691477f834db6ea3935541ec8a18d0c8"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.127/wabocli_linux_amd64-0.0.127.tar.gz"
+      sha256 "be57cd36e5ed79366f6e9421acac58ba4a942a771cb4204f8f5b821f6bc6c350"
     end
   end
 
