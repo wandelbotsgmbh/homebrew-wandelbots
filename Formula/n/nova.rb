2f5182d4c0e38e2aeeb14bdbff41a373d8d91220
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-11-08 10:47:40 +0000

    Merge branch 'feature/update-python-lib' into 'main'
    handle both auth methods
    
    See merge request backend/wos-cli!77
    
    -----------------
    
    update cli formula commit f0d0da0d / version 0.0.126

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.125"
+  version "0.0.126"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.125/wabocli_macos_amd64-0.0.125.tar.gz"
-      sha256 "91dab33e6d9a5093f0180fb10b6fd735235a0d483af863a3e70e18c25e7d92b6"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.126/wabocli_macos_amd64-0.0.126.tar.gz"
+      sha256 "294131848c67aa6dcea4be460a24033a5f684ae8757e7da575e4f455ad079fd4"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.125/wabocli_macos_arm64-0.0.125.tar.gz"
-      sha256 "0d8bcd966d3a9e4e4a5a4949bb748f85c6501e2ff7bd5d77ec1aefb75f170150"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.126/wabocli_macos_arm64-0.0.126.tar.gz"
+      sha256 "ca2afc1e019d76e1653d23661e3c57e6b95f64213d1b7c47c04195c5322d0fa8"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.125/wabocli_linux_amd64-0.0.125.tar.gz"
-      sha256 "4788d2071ac3c4188dc8bbf2c7045feeabfcb7ff2557b419b8b85b24a4584c11"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.126/wabocli_linux_amd64-0.0.126.tar.gz"
+      sha256 "9a09b502b5ba2d6625fe47bce894b2e7691477f834db6ea3935541ec8a18d0c8"
     end
   end
 
