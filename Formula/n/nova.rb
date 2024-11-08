24213b3b502b67f434e4313d3f89c3a46163d69b
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-11-08 16:18:00 +0000

    Merge branch 'feature/improve-cell-handling' into 'main'
    Feature/improve cell handling
    
    See merge request backend/wos-cli!79
    
    -----------------
    
    update cli formula commit deab1764 / version 0.0.128

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.127"
+  version "0.0.128"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.127/wabocli_macos_amd64-0.0.127.tar.gz"
-      sha256 "1c724b6f97dbf465784e1ffafe6fe9e92ac0fb67819c12b652f09ab776023ea3"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.128/wabocli_macos_amd64-0.0.128.tar.gz"
+      sha256 "d83f3e806628dea2d81458c3868a3b36dfc99634b429613792158a9d4b32f9af"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.127/wabocli_macos_arm64-0.0.127.tar.gz"
-      sha256 "0f586518847546e6377baf690224a4458b58ffebcafca4154be5e4f3525c0cab"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.128/wabocli_macos_arm64-0.0.128.tar.gz"
+      sha256 "3de3a85d679d73be446c1dc8435f6efd3ca1369cc8e817a0aaebd5e26e978ddd"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.127/wabocli_linux_amd64-0.0.127.tar.gz"
-      sha256 "be57cd36e5ed79366f6e9421acac58ba4a942a771cb4204f8f5b821f6bc6c350"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.128/wabocli_linux_amd64-0.0.128.tar.gz"
+      sha256 "f2dc11261153c616c856cdc10fe83cbe8beb3e3e844b015c3f4413ef6e07b65e"
     end
   end
 
