5fd7d722b71f13e07a4306325070513069ced6c3
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-11-15 15:19:26 +0000

    Merge branch 'feature/ADX-164-use-gh-catalog' into 'main'
    use gh catalog
    
    See merge request backend/wos-cli!83
    
    -----------------
    
    update cli formula commit 1d980e1e / version 0.0.133

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.132"
+  version "0.0.133"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.132/wabocli_macos_amd64-0.0.132.tar.gz"
-      sha256 "15b85f07fcf54350ddbcd06dc25a0e17ed0725c595fc511e414aaf90cec0fbf4"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.133/wabocli_macos_amd64-0.0.133.tar.gz"
+      sha256 "e4b4a234e0dafa96fdb5a5ca0549993974d2589e58152504a64803eae0648395"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.132/wabocli_macos_arm64-0.0.132.tar.gz"
-      sha256 "cc98e08b4ea187b828e4fb26e5e39db3c7db4c5caa3dca0835ba4e9d5a32ff36"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.133/wabocli_macos_arm64-0.0.133.tar.gz"
+      sha256 "d68e94f15a4a1c241dcf74d88979cdfff425b8aef614df41d918888118ff4560"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.132/wabocli_linux_amd64-0.0.132.tar.gz"
-      sha256 "b0b42a9faf84304af4e8220c14b2f241812085191c29b6ad54c600855401daaa"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.133/wabocli_linux_amd64-0.0.133.tar.gz"
+      sha256 "43efdd8868243ba8cd5a19e23f1a13297e3ce9cd4ff2f2302c60ad3e9a307caf"
     end
   end
 
