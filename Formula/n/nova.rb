5e9e3713b188f285a0714e65abfa476740a70775
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-11-15 15:39:52 +0000

    Merge branch 'bugfix/flags' into 'main'
    version flag
    
    See merge request backend/wos-cli!84
    
    -----------------
    
    update cli formula commit 48904597 / version 0.0.134

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.133"
+  version "0.0.134"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.133/wabocli_macos_amd64-0.0.133.tar.gz"
-      sha256 "e4b4a234e0dafa96fdb5a5ca0549993974d2589e58152504a64803eae0648395"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.134/wabocli_macos_amd64-0.0.134.tar.gz"
+      sha256 "a90c0b57f3e616405b94fd8f8d8366915921a54517524d550f5675d2215ab78f"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.133/wabocli_macos_arm64-0.0.133.tar.gz"
-      sha256 "d68e94f15a4a1c241dcf74d88979cdfff425b8aef614df41d918888118ff4560"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.134/wabocli_macos_arm64-0.0.134.tar.gz"
+      sha256 "eeebe1bb9d9b38ba5e8ce89c9024a1baa51dd466994194f5da871ab100316c53"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.133/wabocli_linux_amd64-0.0.133.tar.gz"
-      sha256 "43efdd8868243ba8cd5a19e23f1a13297e3ce9cd4ff2f2302c60ad3e9a307caf"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.134/wabocli_linux_amd64-0.0.134.tar.gz"
+      sha256 "cc6717e0b3c2c289eefedbc42f0d79c0ac119cfa535fd40f8966304be8f29c80"
     end
   end
 
