ae764ef82c679e81e4a09591a87d36350d168b57
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-11-27 10:53:32 +0000

    Merge branch 'feature/remove-basic-auth-from-template' into 'main'
    feat: remove basic auth from templates
    
    See merge request backend/wos-cli!87
    
    -----------------
    
    update cli formula commit d207aff9 / version 0.0.137

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.136"
+  version "0.0.137"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.136/wabocli_macos_amd64-0.0.136.tar.gz"
-      sha256 "54f7d63df190d09b4f08263ec11e3d3db82e37b2e08ac23dcd556687889fbcce"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.137/wabocli_macos_amd64-0.0.137.tar.gz"
+      sha256 "1c32b28168d53277370a901ab1c3aa1e1775bbf50e3e815574eb04289f84712e"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.136/wabocli_macos_arm64-0.0.136.tar.gz"
-      sha256 "b7e649c9bc8ec06903a85e68f74d462cc31086e3e3e3b74566f1b757510a4d8e"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.137/wabocli_macos_arm64-0.0.137.tar.gz"
+      sha256 "1129aeb24d361d534342701d05772ef77471bb46345e69c0eaa7b2fad86600c2"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.136/wabocli_linux_amd64-0.0.136.tar.gz"
-      sha256 "406870c1e19ea36a34b4a4725e64748467bbfd3f8497396675f221ea2ceab37a"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.137/wabocli_linux_amd64-0.0.137.tar.gz"
+      sha256 "a2f24cba5c132d4645746b238eea55026bd6ac6fb71de9bb17b7ce1a86f6d772"
     end
   end
 
