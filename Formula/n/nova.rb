3ce156517e553d154aa9f37773bcc724c722181f
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-11-28 14:08:21 +0000

    Merge branch 'feature/ADX-172-use-refresh-token' into 'main'
    feat: use refresh token to get new access token before actual login
    
    Closes ADX-172
    
    See merge request backend/wos-cli!88
    
    -----------------
    
    update cli formula commit 811ecf8d / version 0.0.138

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.137"
+  version "0.0.138"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.137/wabocli_macos_amd64-0.0.137.tar.gz"
-      sha256 "1c32b28168d53277370a901ab1c3aa1e1775bbf50e3e815574eb04289f84712e"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.138/wabocli_macos_amd64-0.0.138.tar.gz"
+      sha256 "4e5862e25b15cae09ae62b9e349accfe0231492ecd1b1ee997e386616b5b6ce3"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.137/wabocli_macos_arm64-0.0.137.tar.gz"
-      sha256 "1129aeb24d361d534342701d05772ef77471bb46345e69c0eaa7b2fad86600c2"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.138/wabocli_macos_arm64-0.0.138.tar.gz"
+      sha256 "e45b055a3b3f8862c169f9bdf0e29229a1a9bdc7b04334be664b0965959ec475"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.137/wabocli_linux_amd64-0.0.137.tar.gz"
-      sha256 "a2f24cba5c132d4645746b238eea55026bd6ac6fb71de9bb17b7ce1a86f6d772"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.138/wabocli_linux_amd64-0.0.138.tar.gz"
+      sha256 "25877f53db057e59bbb7542b49d36d2065f336100ecce0c2d8eaeb76798ad528"
     end
   end
 
