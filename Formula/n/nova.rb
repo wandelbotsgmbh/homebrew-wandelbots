67bd5b5ccb3a06b080fc161f555fdd2c59a6abf2
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-12-05 09:22:21 +0000

    Merge branch 'feature/add-args-to-description' into 'main'
    Feature/add args to description
    
    See merge request backend/wos-cli!92
    
    -----------------
    
    update cli formula commit 7ab0542f / version 0.0.142

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.141"
+  version "0.0.142"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.141/wabocli_macos_amd64-0.0.141.tar.gz"
-      sha256 "fa7bd20c2af00c1bf9fe06d09c6bce23c6c3a89d05cb03b85e0d03827f8f62a8"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.142/wabocli_macos_amd64-0.0.142.tar.gz"
+      sha256 "54962e427f157a82c30a3cdf7d014116293f66adfe8e38f3068044efc41cc8fd"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.141/wabocli_macos_arm64-0.0.141.tar.gz"
-      sha256 "250fcdbf7133ee52ad471a0ba5209506e6d3ab464b05527fc824c58e4028cc19"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.142/wabocli_macos_arm64-0.0.142.tar.gz"
+      sha256 "721d500a9274cf1b4f986d16d456e9184f49a822c92eb1783de70515efd59f0b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.141/wabocli_linux_amd64-0.0.141.tar.gz"
-      sha256 "ed063ff8bb1b2eff4f2c90935a4a699996224d286ea171c078facc955bd19dc7"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.142/wabocli_linux_amd64-0.0.142.tar.gz"
+      sha256 "baa5bacc4b0c4a7e6ba6855f9c4bd98109a78ba1f4fc09299cd32040cb1cc960"
     end
   end
 
