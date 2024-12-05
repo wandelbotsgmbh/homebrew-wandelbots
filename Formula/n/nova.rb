8a81bd49a3db54ec345f83899ec58e855c11b4b8
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-12-05 07:43:36 +0000

    Merge branch 'feature/use-catalog-yaml-direct' into 'main'
    feat: use the app manifest without unmarshal to struct
    
    See merge request backend/wos-cli!91
    
    -----------------
    
    update cli formula commit e694ee80 / version 0.0.141

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.140"
+  version "0.0.141"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.140/wabocli_macos_amd64-0.0.140.tar.gz"
-      sha256 "bb3be659f1397bb1add506abc4b80642628cee524671c9b9320d7e1ccc792b82"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.141/wabocli_macos_amd64-0.0.141.tar.gz"
+      sha256 "fa7bd20c2af00c1bf9fe06d09c6bce23c6c3a89d05cb03b85e0d03827f8f62a8"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.140/wabocli_macos_arm64-0.0.140.tar.gz"
-      sha256 "fec61313bd3b85b7c7566a9eade57fc22430253637b3c70c4cdae0018b928d6b"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.141/wabocli_macos_arm64-0.0.141.tar.gz"
+      sha256 "250fcdbf7133ee52ad471a0ba5209506e6d3ab464b05527fc824c58e4028cc19"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.140/wabocli_linux_amd64-0.0.140.tar.gz"
-      sha256 "00cc2d17e7806aaa56902d5ca45b5a2c9453e0e798938adc4012b956ab0be152"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.141/wabocli_linux_amd64-0.0.141.tar.gz"
+      sha256 "ed063ff8bb1b2eff4f2c90935a4a699996224d286ea171c078facc955bd19dc7"
     end
   end
 
