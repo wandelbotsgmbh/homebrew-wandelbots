c9efcb67a4dff0526aeb44a62e9eb430643bec9d
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2024-12-19 10:46:08 +0000

    Merge branch 'feature/NC-773-audience' into 'main'
    use new audience
    
    See merge request backend/wos-cli!93
    
    -----------------
    
    update cli formula commit 1d58c8aa / version 0.0.143

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.142"
+  version "0.0.143"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.142/wabocli_macos_amd64-0.0.142.tar.gz"
-      sha256 "54962e427f157a82c30a3cdf7d014116293f66adfe8e38f3068044efc41cc8fd"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.143/wabocli_macos_amd64-0.0.143.tar.gz"
+      sha256 "d84e61dbdf6d442a38fddf0484b3af4133745dcb60064e15af77c87f7eebb312"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.142/wabocli_macos_arm64-0.0.142.tar.gz"
-      sha256 "721d500a9274cf1b4f986d16d456e9184f49a822c92eb1783de70515efd59f0b"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.143/wabocli_macos_arm64-0.0.143.tar.gz"
+      sha256 "87d90df6b940e528afd3a274fb5eaf12e59e84c0dcc3a9c3b8ea91758037dcb7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.142/wabocli_linux_amd64-0.0.142.tar.gz"
-      sha256 "baa5bacc4b0c4a7e6ba6855f9c4bd98109a78ba1f4fc09299cd32040cb1cc960"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.143/wabocli_linux_amd64-0.0.143.tar.gz"
+      sha256 "4b8dd40e65a795265430f9e8286f963d4c47ee9cec333061a235b64d08766c85"
     end
   end
 
