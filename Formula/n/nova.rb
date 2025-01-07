4b5108d5ef3fd7923beb69f700fdda7065d63c55
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-01-07 10:35:35 +0000

    Merge branch 'feature/output-user-code-directly' into 'main'
    verify user code in console
    
    See merge request backend/wos-cli!96
    
    -----------------
    
    update cli formula commit b9447157 / version 0.0.144

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.143"
+  version "0.0.144"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.143/wabocli_macos_amd64-0.0.143.tar.gz"
-      sha256 "d84e61dbdf6d442a38fddf0484b3af4133745dcb60064e15af77c87f7eebb312"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.144/wabocli_macos_amd64-0.0.144.tar.gz"
+      sha256 "b392fa66c5e744cdd4936c4bb76b66bd82d4320daf77108fce6855f0fb4dc14c"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.143/wabocli_macos_arm64-0.0.143.tar.gz"
-      sha256 "87d90df6b940e528afd3a274fb5eaf12e59e84c0dcc3a9c3b8ea91758037dcb7"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.144/wabocli_macos_arm64-0.0.144.tar.gz"
+      sha256 "5aad0de3918cba4bf4b03c599f68329cf161547ef5c171c1fa14ab7f279b62ff"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.143/wabocli_linux_amd64-0.0.143.tar.gz"
-      sha256 "4b8dd40e65a795265430f9e8286f963d4c47ee9cec333061a235b64d08766c85"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.144/wabocli_linux_amd64-0.0.144.tar.gz"
+      sha256 "04c9f14ec773f121285126e16bea248c160c65c3f790fe4bacfc6943da879f7c"
     end
   end
 
