928d9f476d2f1786d2d6b7674aff4b61c1f07d85
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-01-10 11:18:06 +0000

    Merge branch 'feat/RPS-1043' into 'main'
    feat(RPS-1043): using wandelbots-nova in python template
    
    Closes RPS-1043
    
    See merge request backend/wos-cli!98
    
    -----------------
    
    update cli formula commit 87e5f7d3 / version 0.0.147

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.146"
+  version "0.0.147"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.146/wabocli_macos_amd64-0.0.146.tar.gz"
-      sha256 "153bc77f854311f6a3867c5301cb2d41f344486c7762dd8e73102f7f2b5c94c3"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.147/wabocli_macos_amd64-0.0.147.tar.gz"
+      sha256 "9b2c2fdcfc1d4652a71023236ebaae4ba6a8875e2858f1b6809e670c033b831c"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.146/wabocli_macos_arm64-0.0.146.tar.gz"
-      sha256 "d178c704cfe3adc62880d51d9652407cca48cf30956f31a71ac96e822a75defd"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.147/wabocli_macos_arm64-0.0.147.tar.gz"
+      sha256 "7a8b6dd21f5d2fee2b6bbf6cb5d317e2b660fa6738f27e8e6a121b33f05428c7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.146/wabocli_linux_amd64-0.0.146.tar.gz"
-      sha256 "2d6472c6b2a3d3ead7dca34ebb9462f5922f96f10b942dff2efe9d18da159bb4"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.147/wabocli_linux_amd64-0.0.147.tar.gz"
+      sha256 "51eb43271ef2f67eff5482d9fdc6685e7bc59c4ffcaec019b8388d0669116a01"
     end
   end
 
