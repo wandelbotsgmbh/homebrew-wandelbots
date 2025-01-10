b818086331ed0ec61209df7782e331377a230594
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-01-10 15:59:28 +0000

    Merge branch 'feature/coverage' into 'main'
    adding coverage reports
    
    See merge request backend/wos-cli!100
    
    -----------------
    
    update cli formula commit 66def5fa / version 0.0.149

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.148"
+  version "0.0.149"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.148/wabocli_macos_amd64-0.0.148.tar.gz"
-      sha256 "3109d379da09159aa35a8f0af512e4b33df070f152e5611e9fd7e4a31eb0daa1"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.149/wabocli_macos_amd64-0.0.149.tar.gz"
+      sha256 "2076b6f41d0f0a813c82aa9c2f74f0240e14e647c2f54a74732e07aa5b8fb90e"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.148/wabocli_macos_arm64-0.0.148.tar.gz"
-      sha256 "1d5b344371b44d8444dd62ba85c5bc29d48aa9c9f82c156448912af8557edaa2"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.149/wabocli_macos_arm64-0.0.149.tar.gz"
+      sha256 "68d87d73b15640fbc1a3b4a572eda4dd55757174795c3afd89251fa255f0a0fb"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.148/wabocli_linux_amd64-0.0.148.tar.gz"
-      sha256 "ef5cbcf9cfea9eec714083b5696bea8e3b6b77c4c6adf51fdcab6481960df182"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.149/wabocli_linux_amd64-0.0.149.tar.gz"
+      sha256 "4315eb213aa4003147045058db1626c7bd90ede6d590eaddfb35e2201e963d8f"
     end
   end
 
