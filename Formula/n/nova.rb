c8f5b825afa7db455d92fdecad0e6410083466c8
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-01-14 15:43:26 +0000

    Merge branch 'fix/app-readme' into 'main'
    fix typo
    
    See merge request backend/wos-cli!101
    
    -----------------
    
    update cli formula commit a99c4bcb / version 0.0.151

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.149"
+  version "0.0.151"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.149/wabocli_macos_amd64-0.0.149.tar.gz"
-      sha256 "2076b6f41d0f0a813c82aa9c2f74f0240e14e647c2f54a74732e07aa5b8fb90e"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.151/wabocli_macos_amd64-0.0.151.tar.gz"
+      sha256 "27a3892f110a83b31826334bc776d262277e43532bd9c99e45409ef0ca9c3086"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.149/wabocli_macos_arm64-0.0.149.tar.gz"
-      sha256 "68d87d73b15640fbc1a3b4a572eda4dd55757174795c3afd89251fa255f0a0fb"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.151/wabocli_macos_arm64-0.0.151.tar.gz"
+      sha256 "b082cc276985e35a31e8819228990f6d0b07a6957fcf25f3b76b04d1e7f02d22"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.149/wabocli_linux_amd64-0.0.149.tar.gz"
-      sha256 "4315eb213aa4003147045058db1626c7bd90ede6d590eaddfb35e2201e963d8f"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.151/wabocli_linux_amd64-0.0.151.tar.gz"
+      sha256 "62f6526d5a0e41043e03b909a3b29b1b73c0d38e4bc22e374f0e764f586a0e91"
     end
   end
 
