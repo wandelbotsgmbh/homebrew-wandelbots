2c586ce6afa9428ef3c1c25c5f7f7705187f3f97
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-01-20 16:16:24 +0000

    Merge branch 'fix/python-image' into 'main'
    fix python base image
    
    See merge request backend/wos-cli!104
    
    -----------------
    
    update cli formula commit 55b57d6c / version 0.0.153

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/wabocli"
-  version "0.0.152"
+  version "0.0.153"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.152/wabocli_macos_amd64-0.0.152.tar.gz"
-      sha256 "05a8c94f341ee02baa64079494c9c9fb626b7edbbe9f0bc955fb2ce76a1033ce"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.153/wabocli_macos_amd64-0.0.153.tar.gz"
+      sha256 "38a0a9a5af0a4e30700765eb4383839062ed4d419fa7fb88fb6ba6376eef05a0"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.152/wabocli_macos_arm64-0.0.152.tar.gz"
-      sha256 "ed8c8c986772e56c925df778c06b811eecaf6306d1203d845b734efdeeb92926"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.153/wabocli_macos_arm64-0.0.153.tar.gz"
+      sha256 "a09529fb950b80fb14eeb38f3890223878a09549a9148b2582e8d1d6459aea22"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.152/wabocli_linux_amd64-0.0.152.tar.gz"
-      sha256 "419aaba14f750d066076728cd115c2e9b321ed4ab997f6316ed24a9d2e2b83e5"
+      url "https://github.com/wandelbotsgmbh/wabocli/releases/download/0.0.153/wabocli_linux_amd64-0.0.153.tar.gz"
+      sha256 "ac3998d8fa760bda79cf386f1ab90afbc7f1bc047a8604f3a68e54708c4c89e5"
     end
   end
 
