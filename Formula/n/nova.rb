577fb558017e6d362e4e688e1ba8d7d2c3bad240
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-01-22 11:38:18 +0000

    Merge branch 'feature/get-valid-token' into 'main'
    update oauth and return valid token
    
    See merge request backend/wos-cli!106
    
    -----------------
    
    update cli formula commit ee344d24 / version 0.0.155

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.154"
+  version "0.0.155"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.154/novacli_macos_amd64-0.0.154.tar.gz"
-      sha256 "aee7ae95af733f243e22c1daa4ecd55baf32f3529f99835f2829505037d9d61f"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.155/novacli_macos_amd64-0.0.155.tar.gz"
+      sha256 "36cccafda93b6e15b0d41fd2373b6bfe2e0dc938ea67152d7ff5c3e523637ef6"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.154/novacli_macos_arm64-0.0.154.tar.gz"
-      sha256 "0a10f269a1deb684adf9d108db9997f25526d056aa9b3b6969081a4d6f819158"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.155/novacli_macos_arm64-0.0.155.tar.gz"
+      sha256 "77f008eca37b109d6d76361dba73fc7969d2bd580d908b3aaaecc67030d9562c"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.154/novacli_linux_amd64-0.0.154.tar.gz"
-      sha256 "9370d877c183eda56e9da3602fb4606a38b457c8ee815f39b0dedd318403674a"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.155/novacli_linux_amd64-0.0.155.tar.gz"
+      sha256 "13ac96dde2f8e81a6bc193ece47273a04541b815248b9a4ae33e8e9ef9540313"
     end
   end
 
