5dc6131b8f973388aee3a4e3e0aae25c8da476a1
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-01-30 18:39:48 +0000

    Merge branch 'feature/new-audience-for-all' into 'main'
    new audience for all
    
    See merge request backend/wos-cli!102
    
    -----------------
    
    update cli formula commit 527fb6a3 / version 0.0.156

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.155"
+  version "0.0.156"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.155/novacli_macos_amd64-0.0.155.tar.gz"
-      sha256 "36cccafda93b6e15b0d41fd2373b6bfe2e0dc938ea67152d7ff5c3e523637ef6"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.156/novacli_macos_amd64-0.0.156.tar.gz"
+      sha256 "1c7087c2532c59a0876cc7c565d9d20df48cbdab7025cfbef388dbe51b3929e8"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.155/novacli_macos_arm64-0.0.155.tar.gz"
-      sha256 "77f008eca37b109d6d76361dba73fc7969d2bd580d908b3aaaecc67030d9562c"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.156/novacli_macos_arm64-0.0.156.tar.gz"
+      sha256 "e7e90a625f40b70af8c1365eb2a2ed765c7fd10bde3408a97b22d7d5af449440"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.155/novacli_linux_amd64-0.0.155.tar.gz"
-      sha256 "13ac96dde2f8e81a6bc193ece47273a04541b815248b9a4ae33e8e9ef9540313"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.156/novacli_linux_amd64-0.0.156.tar.gz"
+      sha256 "e3dab6deb86a6451ab3ca89d79fd95f81bd21c13bd0321d488164ab27a18f722"
     end
   end
 
