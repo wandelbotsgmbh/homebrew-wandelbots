6c3a6d8c3c06246625bace74e9714ca1f1b0a7a8
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-02-07 15:29:37 +0000

    Merge branch 'bugfix/ADX-342-check-app-dir' into 'main'
    prompting the user for override
    
    Closes ADX-342
    
    See merge request backend/wos-cli!109
    
    -----------------
    
    update cli formula commit 20e5e7f1 / version 0.0.159

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.158"
+  version "0.0.159"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.158/novacli_macos_amd64-0.0.158.tar.gz"
-      sha256 "264203c96a63c9e6f370019ebfe75e493fc14d893b12d0582a561d0f445846eb"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.159/novacli_macos_amd64-0.0.159.tar.gz"
+      sha256 "9b03c4d629d0e397e153d10959b1675a83ffbab58d5d60a375aab65d125a130a"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.158/novacli_macos_arm64-0.0.158.tar.gz"
-      sha256 "f718b3efabde199f8a6e1921969ba3703fab0ed82b9464be949808fd19ff8486"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.159/novacli_macos_arm64-0.0.159.tar.gz"
+      sha256 "6507a9486669723197754d1da0cee30c8d34b6c9453e441b7cf511eb45012b09"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.158/novacli_linux_amd64-0.0.158.tar.gz"
-      sha256 "522e527681ca8d119ef15b8acc75bc934bfe6ec788a57d3c9de47fa563795f1d"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.159/novacli_linux_amd64-0.0.159.tar.gz"
+      sha256 "ccdb107c93f137922fb517f9671838e47669683dd9484b58af54b5ac62048ad2"
     end
   end
 
