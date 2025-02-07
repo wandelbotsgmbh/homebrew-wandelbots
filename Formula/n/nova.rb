1546df4ab7142f6a4b2638a62f9e24d4355fd0d0
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-02-07 11:33:21 +0000

    Merge branch 'feature/stoplight-auth' into 'main'
    use creds from host
    
    See merge request backend/wos-cli!108
    
    -----------------
    
    update cli formula commit 73badcd0 / version 0.0.158

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.157"
+  version "0.0.158"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.157/novacli_macos_amd64-0.0.157.tar.gz"
-      sha256 "909e3e6f56f2a6cf444b5b7f2ca3e21019f0ddb2c3fae120365d5f58fb59d599"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.158/novacli_macos_amd64-0.0.158.tar.gz"
+      sha256 "264203c96a63c9e6f370019ebfe75e493fc14d893b12d0582a561d0f445846eb"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.157/novacli_macos_arm64-0.0.157.tar.gz"
-      sha256 "1245ad378cc40833012df540b33eabfa9c6e1cada761aadce2da27b4cd0411f8"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.158/novacli_macos_arm64-0.0.158.tar.gz"
+      sha256 "f718b3efabde199f8a6e1921969ba3703fab0ed82b9464be949808fd19ff8486"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.157/novacli_linux_amd64-0.0.157.tar.gz"
-      sha256 "533919853ec180462396ac2fbd6fbd66ab2e3dca93595649bd26ddffede49029"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.158/novacli_linux_amd64-0.0.158.tar.gz"
+      sha256 "522e527681ca8d119ef15b8acc75bc934bfe6ec788a57d3c9de47fa563795f1d"
     end
   end
 
