13ccf0258b58827d1eb5a3602fee83adf672526e
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-02-10 15:15:08 +0000

    Merge branch 'bugfix/ADX-326-python-auth' into 'main'
    Update dependencies in poetry.lock and pyproject.toml.tpl for wandelbots-nova
    
    Closes ADX-326
    
    See merge request backend/wos-cli!110
    
    -----------------
    
    update cli formula commit da977b77 / version 0.0.160

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.159"
+  version "0.0.160"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.159/novacli_macos_amd64-0.0.159.tar.gz"
-      sha256 "9b03c4d629d0e397e153d10959b1675a83ffbab58d5d60a375aab65d125a130a"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.160/novacli_macos_amd64-0.0.160.tar.gz"
+      sha256 "8a52e26489b0c7d7adb67dc449913415cde853154202a39ca18c10c414c60e71"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.159/novacli_macos_arm64-0.0.159.tar.gz"
-      sha256 "6507a9486669723197754d1da0cee30c8d34b6c9453e441b7cf511eb45012b09"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.160/novacli_macos_arm64-0.0.160.tar.gz"
+      sha256 "da1f86f18556a733da89ba6d98e63517f845991c1ef6a344b937f034f81152ba"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.159/novacli_linux_amd64-0.0.159.tar.gz"
-      sha256 "ccdb107c93f137922fb517f9671838e47669683dd9484b58af54b5ac62048ad2"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.160/novacli_linux_amd64-0.0.160.tar.gz"
+      sha256 "742d411289c41ea35bf4381fce770353185f37c2bbd95ce4a131adb0aca33f3a"
     end
   end
 
