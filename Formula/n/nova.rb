1f1920b2f1b1dd6596994e101f275b863e36e06b
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-02-25 09:24:39 +0000

    fix: version file and update package.json.tpl dependencies for nextjs app
    -----------------
    
    update cli formula commit 4ee9c673 / version 0.0.162

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.161"
+  version "0.0.162"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.161/novacli_macos_amd64-0.0.161.tar.gz"
-      sha256 "42f4bb22ede68ea36f58d73049e9bd3c7481043b1227888b7d03a7919592d62b"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.162/novacli_macos_amd64-0.0.162.tar.gz"
+      sha256 "d644646667a4e7697f29b76d578ed2b3b75a26ab60d7f72c610353f821412537"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.161/novacli_macos_arm64-0.0.161.tar.gz"
-      sha256 "5643250e8ae8f42699ec37005c30b9dab754dfa680f2f24509427fc5e42a8cd0"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.162/novacli_macos_arm64-0.0.162.tar.gz"
+      sha256 "4995e04279c0e25ac5a8e1d1fcd1205e731652c1332217f629b583cd9814c0e3"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.161/novacli_linux_amd64-0.0.161.tar.gz"
-      sha256 "47d9d70c2bc0d42abc4b1033327a54001be6526d7ab5733641291306d4dfabf9"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.162/novacli_linux_amd64-0.0.162.tar.gz"
+      sha256 "e452a31197099659fad7eb89e64f1219f37cd43d9a30df3dd32eba9d9f7278c1"
     end
   end
 
