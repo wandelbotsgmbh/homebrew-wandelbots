90b8d1f92b25c669110da984b6a716169ae7b285
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-04-09 13:51:15 +0000

    Merge branch 'feature/ADX-508-update-nova-js' into 'main'
    fix: Update dependencies and refactor imports for nova-js
    
    Closes ADX-508
    
    See merge request backend/wos-cli!115
    
    -----------------
    
    update cli formula commit 89f33275 / version 0.0.166

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.165"
+  version "0.0.166"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.165/novacli_macos_amd64-0.0.165.tar.gz"
-      sha256 "c77ad76923dc77b5d1ba0a13dd69d3715b5b93b3b9c24f3461ed064bc77faa96"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.166/novacli_macos_amd64-0.0.166.tar.gz"
+      sha256 "442e9e2f28995b78ba400b4a93e07fc6de0a2002ae5d9ace36d6083986d71e87"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.165/novacli_macos_arm64-0.0.165.tar.gz"
-      sha256 "56fe490e12081b36548b50afc6b3be064f20a55fe8b053a8086c66cb4f96ba7a"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.166/novacli_macos_arm64-0.0.166.tar.gz"
+      sha256 "e597f2a3ad1f613293608ee8d1556d4dea019980c27ee00a1a98f4d31990e0b3"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.165/novacli_linux_amd64-0.0.165.tar.gz"
-      sha256 "9514abdb10ed851d3c6c63866512f500086900ae9e3a06a0f6ac9c58579be607"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.166/novacli_linux_amd64-0.0.166.tar.gz"
+      sha256 "d08df0a3d18b1df5f02565156f00d59fe91cc43b4a3795e55f15bbcdaab5800f"
     end
   end
 
