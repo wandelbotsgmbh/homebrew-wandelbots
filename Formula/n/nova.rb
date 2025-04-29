32056eb01590522e71f7e9e9a784db0fc38a7fdc
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-04-29 21:28:43 +0000

    Merge branch 'feature/mcp-server-v2' into 'main'
    Feature/mcp server v2
    
    See merge request backend/wos-cli!117
    
    -----------------
    
    update cli formula commit ae601d90 / version 0.0.167

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.166"
+  version "0.0.167"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.166/novacli_macos_amd64-0.0.166.tar.gz"
-      sha256 "442e9e2f28995b78ba400b4a93e07fc6de0a2002ae5d9ace36d6083986d71e87"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.167/novacli_macos_amd64-0.0.167.tar.gz"
+      sha256 "4a003bba20684b43da050409a1be587faa7de8dbdf058d1097513fe28c996c10"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.166/novacli_macos_arm64-0.0.166.tar.gz"
-      sha256 "e597f2a3ad1f613293608ee8d1556d4dea019980c27ee00a1a98f4d31990e0b3"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.167/novacli_macos_arm64-0.0.167.tar.gz"
+      sha256 "3a3787f4f5869aa42f0b6c8ae27e70da77d241093744f7135bbf32f472ea4361"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.166/novacli_linux_amd64-0.0.166.tar.gz"
-      sha256 "d08df0a3d18b1df5f02565156f00d59fe91cc43b4a3795e55f15bbcdaab5800f"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.167/novacli_linux_amd64-0.0.167.tar.gz"
+      sha256 "92802f28c2d60c43be59c15b74f1aaaf328becfd31cf11d6e41b245be01d9786"
     end
   end
 
