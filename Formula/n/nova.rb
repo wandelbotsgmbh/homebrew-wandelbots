48e10c29a554c4804384afa2f0af580e6f44b2bd
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-04-30 07:56:50 +0000

    Merge branch 'bugfix/replace-download-robot-script' into 'main'
    fix: Implement robot model update functionality with CDN support for typescript project
    
    See merge request backend/wos-cli!118
    
    -----------------
    
    update cli formula commit dba4331e / version 0.0.168

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.167"
+  version "0.0.168"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.167/novacli_macos_amd64-0.0.167.tar.gz"
-      sha256 "4a003bba20684b43da050409a1be587faa7de8dbdf058d1097513fe28c996c10"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.168/novacli_macos_amd64-0.0.168.tar.gz"
+      sha256 "356f1e95eb874101da7d22fb30dd6f0e8609e81a0fda6f63afee0d86e2b779e3"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.167/novacli_macos_arm64-0.0.167.tar.gz"
-      sha256 "3a3787f4f5869aa42f0b6c8ae27e70da77d241093744f7135bbf32f472ea4361"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.168/novacli_macos_arm64-0.0.168.tar.gz"
+      sha256 "af1357119f3f84e39b57f91eab9bb8be6d5dadd77182cc1caba3901f4f051ba2"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.167/novacli_linux_amd64-0.0.167.tar.gz"
-      sha256 "92802f28c2d60c43be59c15b74f1aaaf328becfd31cf11d6e41b245be01d9786"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.168/novacli_linux_amd64-0.0.168.tar.gz"
+      sha256 "58a3b218b26ac36926a723ab95c1b340c254bd8a87f85a06344ae2e10f4f1b1c"
     end
   end
 
