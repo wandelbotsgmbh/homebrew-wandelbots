90a0e28e3b3307c7a4c9656f98d74ef463069f12
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-05-06 10:49:29 +0000

    Merge branch 'fix/temp-disable-push-dev' into 'main'
    fix: try disable prod
    
    See merge request backend/wos-cli!121
    
    -----------------
    
    update cli formula commit 7271eead / version 0.0.171

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.170"
+  version "0.0.171"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.170/novacli_macos_amd64-0.0.170.tar.gz"
-      sha256 "e9e5b4d46e09cc30071f282778d9ed95d0e8c2a80053c90bdface3cb4fd1b290"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.171/novacli_macos_amd64-0.0.171.tar.gz"
+      sha256 "52b82d35c0b2885bfc885affc530ee49fc018fe70c97cf41db0bdacb08bf9318"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.170/novacli_macos_arm64-0.0.170.tar.gz"
-      sha256 "100af0b73f61fdccd850904831d812990676f76b43ea30b80aab611efb3faf08"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.171/novacli_macos_arm64-0.0.171.tar.gz"
+      sha256 "18dd0f9fd217470dd57d85dba09fe8bd02c66f3ac93be434a1c3e66c638ea42e"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.170/novacli_linux_amd64-0.0.170.tar.gz"
-      sha256 "d55c2c0969010cdcde0b739207167e6c4ee7e957726b5bf551ec79676482aabb"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.171/novacli_linux_amd64-0.0.171.tar.gz"
+      sha256 "a1e91a62911446cb7a1b4e8d12bcffa578615c94919c6992311e8a1b4269ece2"
     end
   end
 
