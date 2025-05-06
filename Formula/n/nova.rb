02affaef305f8da50dcb072b233af7c5a27b5cc8
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-05-06 12:23:31 +0000

    Merge branch 'fix/split-deploy-stages' into 'main'
    fix: split stages
    
    See merge request backend/wos-cli!122
    
    -----------------
    
    update cli formula commit c6afabb2 / version 0.0.172

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.171"
+  version "0.0.172"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.171/novacli_macos_amd64-0.0.171.tar.gz"
-      sha256 "52b82d35c0b2885bfc885affc530ee49fc018fe70c97cf41db0bdacb08bf9318"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.172/novacli_macos_amd64-0.0.172.tar.gz"
+      sha256 "cf901a6480da575644964be4e323d06088055d8a2f150791905a4bf0e837be53"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.171/novacli_macos_arm64-0.0.171.tar.gz"
-      sha256 "18dd0f9fd217470dd57d85dba09fe8bd02c66f3ac93be434a1c3e66c638ea42e"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.172/novacli_macos_arm64-0.0.172.tar.gz"
+      sha256 "0bf31edd02b5585289a43dade2cad55056b78e133174f4874d770f7cc74e35c7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.171/novacli_linux_amd64-0.0.171.tar.gz"
-      sha256 "a1e91a62911446cb7a1b4e8d12bcffa578615c94919c6992311e8a1b4269ece2"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.172/novacli_linux_amd64-0.0.172.tar.gz"
+      sha256 "150757c37f1aad929d4dce09cdf2dcd2fbf75de1013a4a74c4630d75f2d63703"
     end
   end
 
