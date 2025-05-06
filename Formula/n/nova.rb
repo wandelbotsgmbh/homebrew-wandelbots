66de201bb805e5d2e8554f2d02f5d8485879ae76
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-05-06 10:11:17 +0000

    Merge branch 'fix/azureupload' into 'main'
    fix: upload to azure storage account
    
    See merge request backend/wos-cli!120
    
    -----------------
    
    update cli formula commit f7ef4ea2 / version 0.0.170

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.169"
+  version "0.0.170"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.169/novacli_macos_amd64-0.0.169.tar.gz"
-      sha256 "4bcbb0aac8cd34bdc9b22fd5c4d3b29b1d7b6c40ff417ef27b04c63419aee544"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.170/novacli_macos_amd64-0.0.170.tar.gz"
+      sha256 "e9e5b4d46e09cc30071f282778d9ed95d0e8c2a80053c90bdface3cb4fd1b290"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.169/novacli_macos_arm64-0.0.169.tar.gz"
-      sha256 "ba25dfe1f42d6779bfe5ee9dc14f1b162901e29e855a4e05c7eb7a378745e38c"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.170/novacli_macos_arm64-0.0.170.tar.gz"
+      sha256 "100af0b73f61fdccd850904831d812990676f76b43ea30b80aab611efb3faf08"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.169/novacli_linux_amd64-0.0.169.tar.gz"
-      sha256 "be0e177c555c7e027dc9df9d9d7454c23bbf53f41f449d0f9087e741cfac6a7c"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.170/novacli_linux_amd64-0.0.170.tar.gz"
+      sha256 "d55c2c0969010cdcde0b739207167e6c4ee7e957726b5bf551ec79676482aabb"
     end
   end
 
