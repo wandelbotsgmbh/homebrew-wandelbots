e9a61a8628d52f15a9589f46a1930aeb5095cc21
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-05-06 06:12:41 +0000

    Merge branch 'fix/update-az' into 'main'
    fix: Remove temporary allow failure for release-github and update azure-cli image version
    
    See merge request backend/wos-cli!119
    
    -----------------
    
    update cli formula commit d088b399 / version 0.0.169

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.168"
+  version "0.0.169"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.168/novacli_macos_amd64-0.0.168.tar.gz"
-      sha256 "356f1e95eb874101da7d22fb30dd6f0e8609e81a0fda6f63afee0d86e2b779e3"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.169/novacli_macos_amd64-0.0.169.tar.gz"
+      sha256 "4bcbb0aac8cd34bdc9b22fd5c4d3b29b1d7b6c40ff417ef27b04c63419aee544"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.168/novacli_macos_arm64-0.0.168.tar.gz"
-      sha256 "af1357119f3f84e39b57f91eab9bb8be6d5dadd77182cc1caba3901f4f051ba2"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.169/novacli_macos_arm64-0.0.169.tar.gz"
+      sha256 "ba25dfe1f42d6779bfe5ee9dc14f1b162901e29e855a4e05c7eb7a378745e38c"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.168/novacli_linux_amd64-0.0.168.tar.gz"
-      sha256 "58a3b218b26ac36926a723ab95c1b340c254bd8a87f85a06344ae2e10f4f1b1c"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.169/novacli_linux_amd64-0.0.169.tar.gz"
+      sha256 "be0e177c555c7e027dc9df9d9d7454c23bbf53f41f449d0f9087e741cfac6a7c"
     end
   end
 
