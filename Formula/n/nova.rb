5b72cabc49a6c188cc84bf34f75c4c70cf8bd992
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-05-12 21:00:31 +0000

    Merge branch 'feature/gh-release-title' into 'main'
    Feature/gh release title
    
    See merge request backend/wos-cli!124
    
    -----------------
    
    update cli formula commit ff9759d1 / version 0.0.174

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.173"
+  version "0.0.174"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.173/novacli_macos_amd64-0.0.173.tar.gz"
-      sha256 "67dd5f59b8338e040090ace76f414ad5093d0d36372f63de50be9a5abba046c8"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.174/novacli_macos_amd64-0.0.174.tar.gz"
+      sha256 "471104e88a65fcd34ae7908b816bf25eb6d6bd5a0b98a16a95626b5cbd10ae7b"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.173/novacli_macos_arm64-0.0.173.tar.gz"
-      sha256 "213fe25999f8de7f1d8ed76ffc3165f496f8621e2b9c51b3ef4dd6e2c737685e"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.174/novacli_macos_arm64-0.0.174.tar.gz"
+      sha256 "01d37ba3eed2eaa3063a23957a17dc478506c8280caf1d8a134faaaf53049ad8"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.173/novacli_linux_amd64-0.0.173.tar.gz"
-      sha256 "8f7e68c2b3444e2645643ebcee24abfaba56e3a8747bca9da31a1b0fe910157e"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.174/novacli_linux_amd64-0.0.174.tar.gz"
+      sha256 "e3075caaff29dc55f0e14f82640e93af8bff071e304d40b00e5f44169ddac9ce"
     end
   end
 
