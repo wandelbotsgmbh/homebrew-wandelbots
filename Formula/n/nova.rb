64e311837f0b053c1137407c78b3f02c8a48c5bb
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-05-12 20:12:45 +0000

    Merge branch 'bugfix/adx-583-include-storage' into 'main'
    fix: update YAML tags in NovaApp structs and improve mapping handling
    
    See merge request backend/wos-cli!123
    
    -----------------
    
    update cli formula commit 0100beb7 / version 0.0.173

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.172"
+  version "0.0.173"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.172/novacli_macos_amd64-0.0.172.tar.gz"
-      sha256 "cf901a6480da575644964be4e323d06088055d8a2f150791905a4bf0e837be53"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.173/novacli_macos_amd64-0.0.173.tar.gz"
+      sha256 "67dd5f59b8338e040090ace76f414ad5093d0d36372f63de50be9a5abba046c8"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.172/novacli_macos_arm64-0.0.172.tar.gz"
-      sha256 "0bf31edd02b5585289a43dade2cad55056b78e133174f4874d770f7cc74e35c7"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.173/novacli_macos_arm64-0.0.173.tar.gz"
+      sha256 "213fe25999f8de7f1d8ed76ffc3165f496f8621e2b9c51b3ef4dd6e2c737685e"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.172/novacli_linux_amd64-0.0.172.tar.gz"
-      sha256 "150757c37f1aad929d4dce09cdf2dcd2fbf75de1013a4a74c4630d75f2d63703"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.173/novacli_linux_amd64-0.0.173.tar.gz"
+      sha256 "8f7e68c2b3444e2645643ebcee24abfaba56e3a8747bca9da31a1b0fe910157e"
     end
   end
 
