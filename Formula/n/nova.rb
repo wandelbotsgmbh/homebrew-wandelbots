fd4b7ea036ebdeb4f4e466d3ed1aa3eba278f8a5
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-06-20 16:38:38 +0000

    Merge branch 'feature/proper-cleanup' into 'main'
    Feature/proper cleanup
    
    See merge request backend/wos-cli!131
    
    -----------------
    
    update cli formula commit 78e4d5d8 / version 0.0.181

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.180"
+  version "0.0.181"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.180/novacli_macos_amd64-0.0.180.tar.gz"
-      sha256 "919b7285dd67807f50dc3d9f274e10b4f32cd11fe76c1c709b2f78b4f6cb9f14"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.181/novacli_macos_amd64-0.0.181.tar.gz"
+      sha256 "8f2a65236f2f8b4ca0861fb6d1c5c424ec36ccceae6de27f2b05cc2bf286ad24"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.180/novacli_macos_arm64-0.0.180.tar.gz"
-      sha256 "74cc9e709b48689162260c11b43fb5b633b435b441bc3441a5a9cf9ba17b5800"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.181/novacli_macos_arm64-0.0.181.tar.gz"
+      sha256 "0092fd07a4ff2fd5043a1dd51ff240fa3e6cbaaab25bf536cac8ee5e8f271acc"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.180/novacli_linux_amd64-0.0.180.tar.gz"
-      sha256 "9994fdec90a052036004160f9ce83fe10187e99c3da5ecd8fa7ec04d31f2465d"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.181/novacli_linux_amd64-0.0.181.tar.gz"
+      sha256 "e9c62b7acbcdd11a1081088faf68b1be95607b599a6388755e62450d8ff25413"
     end
   end
 
