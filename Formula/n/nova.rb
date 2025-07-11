5c99602a66b5496cc35daf88d7e9b29a5bb3238e
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-07-11 09:10:24 +0000

    Merge branch 'add-novax-app-template' into 'main'
    feat: add novax app template
    
    See merge request backend/wos-cli!132
    
    -----------------
    
    update cli formula commit eb9fa2c0 / version 0.0.182

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.181"
+  version "0.0.182"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.181/novacli_macos_amd64-0.0.181.tar.gz"
-      sha256 "8f2a65236f2f8b4ca0861fb6d1c5c424ec36ccceae6de27f2b05cc2bf286ad24"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.182/novacli_macos_amd64-0.0.182.tar.gz"
+      sha256 "d17a02b959d8b677744cd015c83cfad03ccab337194d9dd005c27929c035ac2b"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.181/novacli_macos_arm64-0.0.181.tar.gz"
-      sha256 "0092fd07a4ff2fd5043a1dd51ff240fa3e6cbaaab25bf536cac8ee5e8f271acc"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.182/novacli_macos_arm64-0.0.182.tar.gz"
+      sha256 "5ea30e8c8d1472d2d20f139404934cd7fb4cc40516b773ba303cba8c2cb242bf"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.181/novacli_linux_amd64-0.0.181.tar.gz"
-      sha256 "e9c62b7acbcdd11a1081088faf68b1be95607b599a6388755e62450d8ff25413"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.182/novacli_linux_amd64-0.0.182.tar.gz"
+      sha256 "e8ede497f7757a256c1471e92170c1b34bf18a0c01b835f8e81d020709309458"
     end
   end
 
