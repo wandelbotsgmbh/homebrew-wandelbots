15102e815384cfc01c164fff5a0babff4a666113
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-07-31 13:55:11 +0000

    Merge branch 'feature/instance-creating-timeout' into 'main'
    instance creation takes more than 8 minutes
    
    See merge request backend/wos-cli!136
    
    -----------------
    
    update cli formula commit f50f3ce1 / version 0.0.185

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.184"
+  version "0.0.185"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.184/novacli_macos_amd64-0.0.184.tar.gz"
-      sha256 "e88c53ec8aa1238bae8775325b15ee0118657f3b70e46ebe3cb2a9b8a3db24c4"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.185/novacli_macos_amd64-0.0.185.tar.gz"
+      sha256 "4b64799a70c1eb10d7efc04105f54ba8eb39476229f1fc299ba4ecd433d05b46"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.184/novacli_macos_arm64-0.0.184.tar.gz"
-      sha256 "2772b9c925f092ec4c557ead8a5833cf8155e88a593501f82cdffc4a5102b8b2"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.185/novacli_macos_arm64-0.0.185.tar.gz"
+      sha256 "4265fb29d160f3ae0e82e74cf460d82f38a1e6a0ae8e899d9006373501c959a6"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.184/novacli_linux_amd64-0.0.184.tar.gz"
-      sha256 "254faf0523da031147e6df7e4f35a06f30fdfb852e3dfec6bab8c840fb1dfd61"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.185/novacli_linux_amd64-0.0.185.tar.gz"
+      sha256 "29d758d2a7eb350020ada2c9f33e4f981d43a478efc3b4afa48e8d0bc0ce129d"
     end
   end
 
