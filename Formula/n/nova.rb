6dc15594bff36555faa9bc7b0e80075d6d0c3475
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-08-01 09:13:35 +0000

    Merge branch 'feature/clean-instance' into 'main'
    fix: proper cleanup when instance creation fails
    
    See merge request backend/wos-cli!137
    
    -----------------
    
    update cli formula commit d127241a / version 0.0.186

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.185"
+  version "0.0.186"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.185/novacli_macos_amd64-0.0.185.tar.gz"
-      sha256 "4b64799a70c1eb10d7efc04105f54ba8eb39476229f1fc299ba4ecd433d05b46"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.186/novacli_macos_amd64-0.0.186.tar.gz"
+      sha256 "7af0fae98861632ca15d79d146fad0d0c754856f7b910fb4e745e34c9eb24018"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.185/novacli_macos_arm64-0.0.185.tar.gz"
-      sha256 "4265fb29d160f3ae0e82e74cf460d82f38a1e6a0ae8e899d9006373501c959a6"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.186/novacli_macos_arm64-0.0.186.tar.gz"
+      sha256 "57ed957253ed6c0c87169f87f19bfe1c6048d06824a92bc3df2362848e2020f6"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.185/novacli_linux_amd64-0.0.185.tar.gz"
-      sha256 "29d758d2a7eb350020ada2c9f33e4f981d43a478efc3b4afa48e8d0bc0ce129d"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.186/novacli_linux_amd64-0.0.186.tar.gz"
+      sha256 "1342f87b41c55e49318a76d7e83ccec71392683d8d240b938461b827e5aaf746"
     end
   end
 
