d93f96c65e43478ae6b3622fa422b5d429b0bba3
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-08-05 18:01:40 +0000

    Merge branch 'update-ai-intructions' into 'main'
    fix: rename copilot file
    
    See merge request backend/wos-cli!139
    
    -----------------
    
    update cli formula commit f95303d7 / version 0.0.189

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.188"
+  version "0.0.189"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.188/novacli_macos_amd64-0.0.188.tar.gz"
-      sha256 "a4e7e2c259afe3b3191603c74489aba13fe92c390a87506fd3dce2b2c2ca5966"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.189/novacli_macos_amd64-0.0.189.tar.gz"
+      sha256 "a78d7dd79edc062e89d04341f8d0f7497cf1c82085e8ad26ebdb2fdc02f662ae"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.188/novacli_macos_arm64-0.0.188.tar.gz"
-      sha256 "143f37bfd496e59886621348ab81b2d500c98ac34cfa88dad41707f2ef4883be"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.189/novacli_macos_arm64-0.0.189.tar.gz"
+      sha256 "b1a8fa22d219240fae3aeaf194182d41a1790c4721c8210ec1a39d332f38e088"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.188/novacli_linux_amd64-0.0.188.tar.gz"
-      sha256 "c9ccc2bf9a6a6ae18abf6b1cb7942b53d33b82c3ea9e2bca5fe36d9fd70818b0"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.189/novacli_linux_amd64-0.0.189.tar.gz"
+      sha256 "29ccf60917df5a16356891ab7109c88b6d2a1fd59b86212810479786f2cbea33"
     end
   end
 
