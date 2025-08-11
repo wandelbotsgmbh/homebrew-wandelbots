913c60b314ff0de4bb9b6aee6eaab4577cb8f209
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-08-11 07:10:37 +0000

    Merge branch 'update-ai-intructions' into 'main'
    fix: Update ai intructions
    
    See merge request backend/wos-cli!140
    
    -----------------
    
    update cli formula commit e309df29 / version 0.0.190

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.189"
+  version "0.0.190"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.189/novacli_macos_amd64-0.0.189.tar.gz"
-      sha256 "a78d7dd79edc062e89d04341f8d0f7497cf1c82085e8ad26ebdb2fdc02f662ae"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.190/novacli_macos_amd64-0.0.190.tar.gz"
+      sha256 "2abf0d20584f476d2aee22933c979b001546df3d19bdf63670a92cad36329227"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.189/novacli_macos_arm64-0.0.189.tar.gz"
-      sha256 "b1a8fa22d219240fae3aeaf194182d41a1790c4721c8210ec1a39d332f38e088"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.190/novacli_macos_arm64-0.0.190.tar.gz"
+      sha256 "1710021cad49ad6dea318ac957e30cd9e1dcac6852b1eaf5d08ccd9e8a636a74"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.189/novacli_linux_amd64-0.0.189.tar.gz"
-      sha256 "29ccf60917df5a16356891ab7109c88b6d2a1fd59b86212810479786f2cbea33"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.190/novacli_linux_amd64-0.0.190.tar.gz"
+      sha256 "9c2f7b9317c2242e38f1132bca11feeadabdba4a2045ccdbf82949c17eec7d7b"
     end
   end
 
