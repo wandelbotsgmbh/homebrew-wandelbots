de9762a29277c1dc1a9c5f2543a79c2a66010e1c
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-08-22 12:23:43 +0000

    Merge branch 'NDX-215-remove-beta-label-from-template-app' into 'main'
    NDX-215 remove beta flag from image for typescript template
    
    Closes NDX-215
    
    See merge request backend/wos-cli!143
    
    -----------------
    
    update cli formula commit 2b0a02fc / version 0.0.191

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.190"
+  version "0.0.191"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.190/novacli_macos_amd64-0.0.190.tar.gz"
-      sha256 "2abf0d20584f476d2aee22933c979b001546df3d19bdf63670a92cad36329227"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.191/novacli_macos_amd64-0.0.191.tar.gz"
+      sha256 "3fa8212b6ec61cd8a8380fe90ff9d26bf3ff071272a446fd2374eb9b6bfdf1ad"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.190/novacli_macos_arm64-0.0.190.tar.gz"
-      sha256 "1710021cad49ad6dea318ac957e30cd9e1dcac6852b1eaf5d08ccd9e8a636a74"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.191/novacli_macos_arm64-0.0.191.tar.gz"
+      sha256 "27108ee64e0699e832025d402757e41293c99097159a4ebc0d763a3e763a4185"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.190/novacli_linux_amd64-0.0.190.tar.gz"
-      sha256 "9c2f7b9317c2242e38f1132bca11feeadabdba4a2045ccdbf82949c17eec7d7b"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.191/novacli_linux_amd64-0.0.191.tar.gz"
+      sha256 "76b0ea5bfdf2f5962e13f02eac2721b3686ac2c480d58f629e6c1b9d7088d8da"
     end
   end
 
