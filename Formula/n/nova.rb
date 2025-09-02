b8db4193fc7fcc0690d91a295ae4377358317949
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-09-02 13:35:40 +0000

    Merge branch 'fix/unpin-sdk-version' into 'main'
    fix: unpin-sdk-version
    
    See merge request backend/wos-cli!146
    
    -----------------
    
    update cli formula commit 052dc62b / version 0.0.193

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.192"
+  version "0.0.193"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.192/novacli_macos_amd64-0.0.192.tar.gz"
-      sha256 "e101ed0d5bbd68800e036427d8d014d420f26057b5f6b6fbd086d6d70b34d38e"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.193/novacli_macos_amd64-0.0.193.tar.gz"
+      sha256 "b8c75fa79d26e6a305b2cbf6f4fdc59759dc4fc7b2f899fe7f79614e27dd358c"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.192/novacli_macos_arm64-0.0.192.tar.gz"
-      sha256 "42ce294c805131512ae87de9f4b0c22592a7f6983ac99b3f64403fdfaefeb618"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.193/novacli_macos_arm64-0.0.193.tar.gz"
+      sha256 "0c03115057b5798086e4cf2ab3e75e52de8b45574e279a88a7464cc35725fbcc"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.192/novacli_linux_amd64-0.0.192.tar.gz"
-      sha256 "46d2a714912f0c82696fb956e8dac1abd04822941f17a84e81923e0022d3b261"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.193/novacli_linux_amd64-0.0.193.tar.gz"
+      sha256 "f84fbf4e9e4a6a774a6839f12ec77445cfc7c2129d996bbf3e82f97059ca9dc6"
     end
   end
 
