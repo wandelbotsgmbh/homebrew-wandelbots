68e5c2d966fa3fb6050016ad253c961e22ff58a1
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-09-05 15:14:48 +0000

    Merge branch 'feature/add-memory-to-resources' into 'main'
    feat: update nova API client references and add memory limit resource
    
    See merge request backend/wos-cli!145
    
    -----------------
    
    update cli formula commit d74e6b48 / version 0.0.194

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.193"
+  version "0.0.194"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.193/novacli_macos_amd64-0.0.193.tar.gz"
-      sha256 "b8c75fa79d26e6a305b2cbf6f4fdc59759dc4fc7b2f899fe7f79614e27dd358c"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.194/novacli_macos_amd64-0.0.194.tar.gz"
+      sha256 "3b011dd6de4a5b186e0c6b9a253501fd44b90c7e4dd14294b7b1df478b837c6c"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.193/novacli_macos_arm64-0.0.193.tar.gz"
-      sha256 "0c03115057b5798086e4cf2ab3e75e52de8b45574e279a88a7464cc35725fbcc"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.194/novacli_macos_arm64-0.0.194.tar.gz"
+      sha256 "0bf0329c77abafa29e28c330967205192ab28494e0d976dd3ed7a4c07ce62660"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.193/novacli_linux_amd64-0.0.193.tar.gz"
-      sha256 "f84fbf4e9e4a6a774a6839f12ec77445cfc7c2129d996bbf3e82f97059ca9dc6"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.194/novacli_linux_amd64-0.0.194.tar.gz"
+      sha256 "d07a2415a32ef75c65bf8c2e013c192df2a6d72e79c05804e587c4c21da39016"
     end
   end
 
