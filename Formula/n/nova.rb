adbe8bee5699226a4b1d7a76cfbd692e84295739
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-09-05 18:24:16 +0000

    Merge branch 'feature/golang-app' into 'main'
    feat: golang app
    
    See merge request backend/wos-cli!147
    
    -----------------
    
    update cli formula commit 66213f05 / version 0.0.195

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.194"
+  version "0.0.195"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.194/novacli_macos_amd64-0.0.194.tar.gz"
-      sha256 "3b011dd6de4a5b186e0c6b9a253501fd44b90c7e4dd14294b7b1df478b837c6c"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.195/novacli_macos_amd64-0.0.195.tar.gz"
+      sha256 "53ced81c0361c8e79642e7d5b2e9770054a60e474002ae5d8225e9789e41b0ca"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.194/novacli_macos_arm64-0.0.194.tar.gz"
-      sha256 "0bf0329c77abafa29e28c330967205192ab28494e0d976dd3ed7a4c07ce62660"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.195/novacli_macos_arm64-0.0.195.tar.gz"
+      sha256 "054dca157d496583fe83e18a91b56382b11090c47e2491f1b4d5241699bdb5e0"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.194/novacli_linux_amd64-0.0.194.tar.gz"
-      sha256 "d07a2415a32ef75c65bf8c2e013c192df2a6d72e79c05804e587c4c21da39016"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.195/novacli_linux_amd64-0.0.195.tar.gz"
+      sha256 "851c7e2d2a85e0bb6429cb562f8c031fe3e56b726e07d53d1ee45d5884d4b979"
     end
   end
 
