e60113614e0a58dbf62fe5ffcded07ad48f46470
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-09-09 07:34:11 +0000

    Merge branch 'reproducable-builds' into 'main'
    fix: pin sdk version for reproducable builds
    
    See merge request backend/wos-cli!148
    
    -----------------
    
    update cli formula commit ea8b9216 / version 0.0.196

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.195"
+  version "0.0.196"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.195/novacli_macos_amd64-0.0.195.tar.gz"
-      sha256 "53ced81c0361c8e79642e7d5b2e9770054a60e474002ae5d8225e9789e41b0ca"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.196/novacli_macos_amd64-0.0.196.tar.gz"
+      sha256 "cfba8b37eb9032597ecdbeb552b2db9f7c4f4d12a5170f50c682243db92eee17"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.195/novacli_macos_arm64-0.0.195.tar.gz"
-      sha256 "054dca157d496583fe83e18a91b56382b11090c47e2491f1b4d5241699bdb5e0"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.196/novacli_macos_arm64-0.0.196.tar.gz"
+      sha256 "9f6f65986b82c247af08b92f62532c25aa505b349ad8358cf6ca10c7e83c8228"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.195/novacli_linux_amd64-0.0.195.tar.gz"
-      sha256 "851c7e2d2a85e0bb6429cb562f8c031fe3e56b726e07d53d1ee45d5884d4b979"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.196/novacli_linux_amd64-0.0.196.tar.gz"
+      sha256 "77995f39afab7ba5186fb1d5cc6fe3708feeb70e2e6cecf91924ec870af88b76"
     end
   end
 
