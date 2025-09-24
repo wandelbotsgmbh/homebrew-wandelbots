3ba1c86f481b577e96320d9225077f5f0fd32395
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-09-24 19:07:50 +0000

    Merge branch 'bugfix/bump-python-in-novax-program' into 'main'
    fix: update required Python version to 3.11
    
    See merge request backend/wos-cli!150
    
    -----------------
    
    update cli formula commit 80916433 / version 0.0.198

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.197"
+  version "0.0.198"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.197/novacli_macos_amd64-0.0.197.tar.gz"
-      sha256 "1fc0e197d4a65c783ef6485c09fb2cfcba8bcc9e69f5be0bff2981c2e8b238e4"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.198/novacli_macos_amd64-0.0.198.tar.gz"
+      sha256 "b916f19925e369ca7655fe2265af180e182ebb8e737f6fbbdde2536a1a587a6b"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.197/novacli_macos_arm64-0.0.197.tar.gz"
-      sha256 "d3113a1da4e7f90d8ad00cc63a137cf9683d13289d6298c9e97d32d77e5a0259"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.198/novacli_macos_arm64-0.0.198.tar.gz"
+      sha256 "792749603bea274e71c7f738a542a473dc225e9a5a476b69a6ab3bc66f6e795b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.197/novacli_linux_amd64-0.0.197.tar.gz"
-      sha256 "02b87585cc3cd957b0bda0c31d489324bc80583ba9a730284a1f90cd58fe542d"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.198/novacli_linux_amd64-0.0.198.tar.gz"
+      sha256 "4e45af3bea8f551a8cb88a49b12bad1137c73b0c27092f58643da8ea598825b2"
     end
   end
 
