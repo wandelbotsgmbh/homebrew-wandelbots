eeed2c0d6b13eaa5e5653ef992e9d9e5de631889
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-09-24 18:49:29 +0000

    Merge branch 'feature/update-novax-project' into 'main'
    fix: update controller configuration and dependency version
    
    See merge request backend/wos-cli!149
    
    -----------------
    
    update cli formula commit 5b1fd6ac / version 0.0.197

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.196"
+  version "0.0.197"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.196/novacli_macos_amd64-0.0.196.tar.gz"
-      sha256 "cfba8b37eb9032597ecdbeb552b2db9f7c4f4d12a5170f50c682243db92eee17"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.197/novacli_macos_amd64-0.0.197.tar.gz"
+      sha256 "1fc0e197d4a65c783ef6485c09fb2cfcba8bcc9e69f5be0bff2981c2e8b238e4"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.196/novacli_macos_arm64-0.0.196.tar.gz"
-      sha256 "9f6f65986b82c247af08b92f62532c25aa505b349ad8358cf6ca10c7e83c8228"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.197/novacli_macos_arm64-0.0.197.tar.gz"
+      sha256 "d3113a1da4e7f90d8ad00cc63a137cf9683d13289d6298c9e97d32d77e5a0259"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.196/novacli_linux_amd64-0.0.196.tar.gz"
-      sha256 "77995f39afab7ba5186fb1d5cc6fe3708feeb70e2e6cecf91924ec870af88b76"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.197/novacli_linux_amd64-0.0.197.tar.gz"
+      sha256 "02b87585cc3cd957b0bda0c31d489324bc80583ba9a730284a1f90cd58fe542d"
     end
   end
 
