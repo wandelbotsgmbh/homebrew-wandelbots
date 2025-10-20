90286892045e8cdb8729b0765cdba1cba7f70c58
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-10-20 13:59:59 +0000

    Merge branch 'feat/NDX-338' into 'main'
    feat(NDX-338): updated python template project with latest SDK version
    
    Closes NDX-338
    
    See merge request backend/wos-cli!154
    
    -----------------
    
    update cli formula commit 5f852a90 / version 0.0.203

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.202"
+  version "0.0.203"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.202/novacli_macos_amd64-0.0.202.tar.gz"
-      sha256 "fe39af8542830a2256b6aeef61278f0dc280ae766a3068b161d45984c2abc137"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.203/novacli_macos_amd64-0.0.203.tar.gz"
+      sha256 "9d3e061e6902062d2c9668252d58ddd7f26b4478478b194f9125518d28dfa4ba"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.202/novacli_macos_arm64-0.0.202.tar.gz"
-      sha256 "f8eb4b463b3e01fdc8831207b12b4a679d78dc296f23ea68d64842719e7ab57f"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.203/novacli_macos_arm64-0.0.203.tar.gz"
+      sha256 "b06149e09e52a5cdc5392b5bf4cc32132d7c5ae96dfc2b4388e6221aa759f609"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.202/novacli_linux_amd64-0.0.202.tar.gz"
-      sha256 "49a32a14eec6518619e6b1add66b35065c653de0b164ef408cb5c1d6cb45774b"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.203/novacli_linux_amd64-0.0.203.tar.gz"
+      sha256 "ff9944100150627ad3c5f8230f2ebc0c02b06417192b03ce386cd7ce2a36f9bb"
     end
   end
 
