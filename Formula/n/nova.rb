f7ec2002b4a7f080b1dd2bfdacfcdb04cba9a5df
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-10-21 18:11:36 +0000

    Merge branch 'bugfix/use-nova-3-3-0' into 'main'
    fix: use nova 3 3 0
    
    See merge request backend/wos-cli!156
    
    -----------------
    
    update cli formula commit 93c1be74 / version 0.0.205

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.204"
+  version "0.0.205"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.204/novacli_macos_amd64-0.0.204.tar.gz"
-      sha256 "91d19cf0ea2842496df827b83cea0b5c5d9abffac20eb566968c4893d61f32b3"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.205/novacli_macos_amd64-0.0.205.tar.gz"
+      sha256 "cb75a48d42acf36b4b07430708ffe034e6b5ad873c83ddc1d5200c84c4cf31cd"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.204/novacli_macos_arm64-0.0.204.tar.gz"
-      sha256 "4b0467fb3f94dbc0ff6002749c12d7f700d472e26fc21b0fb10b143fb28f4276"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.205/novacli_macos_arm64-0.0.205.tar.gz"
+      sha256 "74aaa665a04a2373a3eab34728bb86815d537651ec16ae23ef18beff5dfefa78"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.204/novacli_linux_amd64-0.0.204.tar.gz"
-      sha256 "c69a1224ddd2f52f140dbf41362d73d91f9f146ab3583da21ecbc97a453ccc77"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.205/novacli_linux_amd64-0.0.205.tar.gz"
+      sha256 "8d7eb120502edb128488db00705daad1423816d8944310ee37ee74ef12c130e0"
     end
   end
 
