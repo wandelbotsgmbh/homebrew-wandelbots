d87744e7e0c6556110ccfabdb30f56a518514c0e
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-10-22 14:27:24 +0000

    Merge branch 'fix/remove-cache' into 'main'
    fix: removed wandelbots cache
    
    See merge request backend/wos-cli!158
    
    -----------------
    
    update cli formula commit 6a0a2f12 / version 0.0.207

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.206"
+  version "0.0.207"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.206/novacli_macos_amd64-0.0.206.tar.gz"
-      sha256 "466bf2154ac7c0a4466aa1558a22b6d0c21ecbc7af926edb64abbbb439f7555c"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.207/novacli_macos_amd64-0.0.207.tar.gz"
+      sha256 "a8f2785d85d88e5dfe4116d9fee21d4786bea9036948b21541a0610178eb4e74"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.206/novacli_macos_arm64-0.0.206.tar.gz"
-      sha256 "b2dbbe2d147078114b16be52f7c335b1e2cca82bf0e628aef3c1f54fb6098e32"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.207/novacli_macos_arm64-0.0.207.tar.gz"
+      sha256 "30f481ddd5dfa3ccf61733b2ee918610b4f9fb7de07b6663acd562e195ed1c2d"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.206/novacli_linux_amd64-0.0.206.tar.gz"
-      sha256 "2d6d1b82f95aa641cf09dcd7616ebe063615de396c226aaa93e8b446e6684f81"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.207/novacli_linux_amd64-0.0.207.tar.gz"
+      sha256 "a58272a4562f6451e2e6082e06d4bf22bcee39e3d27ad56e21450884b168b5c7"
     end
   end
 
