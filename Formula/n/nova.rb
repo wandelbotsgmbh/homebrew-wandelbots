55025d201c1521adf99f1b02b018316669fe2a1a
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-10-23 12:26:58 +0000

    Merge branch 'fix/docs' into 'main'
    fix(NDX-371): fixed docs route using stoplight API
    
    See merge request backend/wos-cli!159
    
    -----------------
    
    update cli formula commit 1ada1d19 / version 0.0.208

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.207"
+  version "0.0.208"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.207/novacli_macos_amd64-0.0.207.tar.gz"
-      sha256 "a8f2785d85d88e5dfe4116d9fee21d4786bea9036948b21541a0610178eb4e74"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.208/novacli_macos_amd64-0.0.208.tar.gz"
+      sha256 "a425fe428d5ecac46b82efda6509127addb8035520def05062b3b35000972799"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.207/novacli_macos_arm64-0.0.207.tar.gz"
-      sha256 "30f481ddd5dfa3ccf61733b2ee918610b4f9fb7de07b6663acd562e195ed1c2d"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.208/novacli_macos_arm64-0.0.208.tar.gz"
+      sha256 "1a660e14377e11169eb6c192216d47b4eb891f86cd1d9928f6725d4d65d6c9be"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.207/novacli_linux_amd64-0.0.207.tar.gz"
-      sha256 "a58272a4562f6451e2e6082e06d4bf22bcee39e3d27ad56e21450884b168b5c7"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.208/novacli_linux_amd64-0.0.208.tar.gz"
+      sha256 "96807a5a36c2aef1a7089690146461be282f7122b9ba2cf624f930b0adeee195"
     end
   end
 
