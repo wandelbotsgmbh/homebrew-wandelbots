6f25f6ae67f5fc209ce2fd325468a31acd2d1ccb
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-10-24 09:09:56 +0000

    Merge branch 'feat/launchjson' into 'main'
    feat: updated launch json for python app template
    
    See merge request backend/wos-cli!160
    
    -----------------
    
    update cli formula commit 75e5b5b4 / version 0.0.209

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.208"
+  version "0.0.209"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.208/novacli_macos_amd64-0.0.208.tar.gz"
-      sha256 "a425fe428d5ecac46b82efda6509127addb8035520def05062b3b35000972799"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.209/novacli_macos_amd64-0.0.209.tar.gz"
+      sha256 "de65066e3e40e7bfc064f0d413bfa533b14e10f7d6f3a1162e173d5086f0e9fe"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.208/novacli_macos_arm64-0.0.208.tar.gz"
-      sha256 "1a660e14377e11169eb6c192216d47b4eb891f86cd1d9928f6725d4d65d6c9be"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.209/novacli_macos_arm64-0.0.209.tar.gz"
+      sha256 "30627d7dbe83ab137e3080b9562aabfc77b3c54c8fc401d1a832dc0787db1280"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.208/novacli_linux_amd64-0.0.208.tar.gz"
-      sha256 "96807a5a36c2aef1a7089690146461be282f7122b9ba2cf624f930b0adeee195"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.209/novacli_linux_amd64-0.0.209.tar.gz"
+      sha256 "4d1a46ac0338ed6f569a7e35c6f939ee98f3899d44b014ff42ce285b798085d4"
     end
   end
 
