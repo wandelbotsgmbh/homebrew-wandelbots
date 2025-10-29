77ed0410ce7f966e4c173bc9f09704fbee39f8a4
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-10-29 10:32:34 +0000

    Merge branch 'feature/make-register-program-stand-alone' into 'main'
    feat: add launch configuration for Register Program and main entry point in register_programs.py
    
    See merge request backend/wos-cli!161
    
    -----------------
    
    update cli formula commit 31a8ce2f / version 0.0.210

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.209"
+  version "0.0.210"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.209/novacli_macos_amd64-0.0.209.tar.gz"
-      sha256 "de65066e3e40e7bfc064f0d413bfa533b14e10f7d6f3a1162e173d5086f0e9fe"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.210/novacli_macos_amd64-0.0.210.tar.gz"
+      sha256 "55b488ccd60915a47b3eb575c8def1e0bd43de8257c49c7ff56ccc5775468e05"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.209/novacli_macos_arm64-0.0.209.tar.gz"
-      sha256 "30627d7dbe83ab137e3080b9562aabfc77b3c54c8fc401d1a832dc0787db1280"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.210/novacli_macos_arm64-0.0.210.tar.gz"
+      sha256 "f0d1657ab9193b2d139a1050003ed6efc3ef6a498d7fd84e415740b82b0a299a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.209/novacli_linux_amd64-0.0.209.tar.gz"
-      sha256 "4d1a46ac0338ed6f569a7e35c6f939ee98f3899d44b014ff42ce285b798085d4"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.210/novacli_linux_amd64-0.0.210.tar.gz"
+      sha256 "695ab526c505d77edb4bdac1d9c0e515c9f2672c3bd95a9db3da2db11929b45b"
     end
   end
 
