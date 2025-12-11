2cd2495a0b95529a67112f8d6048f3c0e69abbd9
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2025-12-11 14:27:55 +0000

    Merge branch 'feat/NDX-438' into 'main'
    feat(NDX-438): upgraded NOVA CLI python template to v4.0.0
    
    Closes NDX-438
    
    See merge request backend/wos-cli!167
    
    -----------------
    
    update cli formula commit 68e15e32 / version 0.0.216

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.215"
+  version "0.0.216"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.215/novacli_macos_amd64-0.0.215.tar.gz"
-      sha256 "cd1f44eedda0013c2b35a69fa2292731d28d04b01373801767f470aed16c1e90"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.216/novacli_macos_amd64-0.0.216.tar.gz"
+      sha256 "5fe84d679f35cc0e020fc6092b6720fcf482238c469b02f1b02e3c24fce65389"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.215/novacli_macos_arm64-0.0.215.tar.gz"
-      sha256 "eb3f140e4ff4af539a41e03fb065b11ec3aced199d9764fcd8626b41284d21e8"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.216/novacli_macos_arm64-0.0.216.tar.gz"
+      sha256 "c7eab0d5130553ab60023bd146dd6a051780b4b065970f40c7c8d94e4cc5e023"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.215/novacli_linux_amd64-0.0.215.tar.gz"
-      sha256 "d8659a190e358ca6e779b4cf052a1af199a899e924d25d4c1d312224cbc234b2"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.216/novacli_linux_amd64-0.0.216.tar.gz"
+      sha256 "67faf8051658beddf81841858d87c43e9643edbc29ed724086009ec20d5d4665"
     end
   end
 
