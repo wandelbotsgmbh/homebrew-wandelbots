b3401bba3b0b47563092bbf0e865759bf2087cf6
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2026-01-05 08:55:19 +0000

    Merge branch 'fix/python-template' into 'main'
    fix: fixed start_here program in python app template
    
    See merge request backend/wos-cli!169
    
    -----------------
    
    update cli formula commit 21c0c0b2 / version 0.0.218

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.217"
+  version "0.0.218"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.217/novacli_macos_amd64-0.0.217.tar.gz"
-      sha256 "a6264b8a1c87010c8b2401a0c2e470a357093f81701389c9545c09257bfb2fe6"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.218/novacli_macos_amd64-0.0.218.tar.gz"
+      sha256 "b8d5882b94030a902f3fd8b1d804025c613ab17ffa8f394b7e62b1fecdd62254"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.217/novacli_macos_arm64-0.0.217.tar.gz"
-      sha256 "b3fd0c2f13a6489d22edcb0a5739df9a495cd805f8d554e3dd559e35a28a613f"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.218/novacli_macos_arm64-0.0.218.tar.gz"
+      sha256 "d4b8ab3588f56f79e030651ead8ead1a43e912f9a3e5ac187f7fd8b9c286517a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.217/novacli_linux_amd64-0.0.217.tar.gz"
-      sha256 "f7f1e205369f5951feb8e4be3a369753302ce542d0ac08eee00ae9bbd232d351"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.218/novacli_linux_amd64-0.0.218.tar.gz"
+      sha256 "6f98aff9cfec6f992ef501e67a48c9f4540ff3acf19051c9c2cbfa7d2fb5dcd8"
     end
   end
 
