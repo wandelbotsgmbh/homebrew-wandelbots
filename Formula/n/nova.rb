883701bfb4aa885ea15daae7855ea1be1f463702
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2026-01-06 15:17:12 +0000

    Merge branch 'feat/nextjs-app-template-upgrades' into 'main'
    feat: Upgrade nextjs_app template incl. CVE fix
    
    See merge request backend/wos-cli!171
    
    -----------------
    
    update cli formula commit 6c6a98ae / version 0.0.219

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.218"
+  version "0.0.219"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.218/novacli_macos_amd64-0.0.218.tar.gz"
-      sha256 "b8d5882b94030a902f3fd8b1d804025c613ab17ffa8f394b7e62b1fecdd62254"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.219/novacli_macos_amd64-0.0.219.tar.gz"
+      sha256 "3c8af13406fb918963b47d0c8a8090caf001cff3d3161d4e7add8b0db26e7a50"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.218/novacli_macos_arm64-0.0.218.tar.gz"
-      sha256 "d4b8ab3588f56f79e030651ead8ead1a43e912f9a3e5ac187f7fd8b9c286517a"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.219/novacli_macos_arm64-0.0.219.tar.gz"
+      sha256 "66b3150cdf98f94805f7adcdc180cac37824d18e4b479e743c0f03ff5a60bc58"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.218/novacli_linux_amd64-0.0.218.tar.gz"
-      sha256 "6f98aff9cfec6f992ef501e67a48c9f4540ff3acf19051c9c2cbfa7d2fb5dcd8"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.219/novacli_linux_amd64-0.0.219.tar.gz"
+      sha256 "2aac61e8ea97a99a08edeeb34030505aa17f22d7b09adb2e7f021cfc63d4164c"
     end
   end
 
