a4ea17fb8e1e1b652f5283664b75143c0f8792f2
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2026-01-07 10:18:08 +0000

    Merge branch 'feature/acr-support' into 'main'
    Feature/acr support
    
    See merge request backend/wos-cli!170
    
    -----------------
    
    update cli formula commit 651b00f0 / version 0.0.220

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.219"
+  version "0.0.220"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.219/novacli_macos_amd64-0.0.219.tar.gz"
-      sha256 "3c8af13406fb918963b47d0c8a8090caf001cff3d3161d4e7add8b0db26e7a50"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.220/novacli_macos_amd64-0.0.220.tar.gz"
+      sha256 "86eb63319ed284be50d67afa61189d49fe9eb9d1bf17dbb3bb5855e778f0b8c0"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.219/novacli_macos_arm64-0.0.219.tar.gz"
-      sha256 "66b3150cdf98f94805f7adcdc180cac37824d18e4b479e743c0f03ff5a60bc58"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.220/novacli_macos_arm64-0.0.220.tar.gz"
+      sha256 "36ff82f6b16c1217532ae84af9c88a3f4d45d2e1b6df4480249a5ca976030062"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.219/novacli_linux_amd64-0.0.219.tar.gz"
-      sha256 "2aac61e8ea97a99a08edeeb34030505aa17f22d7b09adb2e7f021cfc63d4164c"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.220/novacli_linux_amd64-0.0.220.tar.gz"
+      sha256 "42be0f2ecbae96a59ccb6ed310f7d97aadfb674e94832765109e5b3b9d756806"
     end
   end
 
