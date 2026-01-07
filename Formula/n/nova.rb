331e2fc7f71a9dbde741a65a947ec0f2ba024edc
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2026-01-07 13:50:00 +0000

    Merge branch 'fix/nextjs-startup-optimization' into 'main'
    fix: Optimize nextjs app startup script
    
    See merge request backend/wos-cli!172
    
    -----------------
    
    update cli formula commit bff62fa0 / version 0.0.221

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.220"
+  version "0.0.221"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.220/novacli_macos_amd64-0.0.220.tar.gz"
-      sha256 "86eb63319ed284be50d67afa61189d49fe9eb9d1bf17dbb3bb5855e778f0b8c0"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.221/novacli_macos_amd64-0.0.221.tar.gz"
+      sha256 "e63922e9e08fd4094cc732e7bb72cdd68087fd3f7fc665554a50a124155ffdb4"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.220/novacli_macos_arm64-0.0.220.tar.gz"
-      sha256 "36ff82f6b16c1217532ae84af9c88a3f4d45d2e1b6df4480249a5ca976030062"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.221/novacli_macos_arm64-0.0.221.tar.gz"
+      sha256 "9dbf2ce0a57c42ebe69327869fceebcced0b65f1f5496e6c69e4d6d656b1ceaa"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.220/novacli_linux_amd64-0.0.220.tar.gz"
-      sha256 "42be0f2ecbae96a59ccb6ed310f7d97aadfb674e94832765109e5b3b9d756806"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.221/novacli_linux_amd64-0.0.221.tar.gz"
+      sha256 "c29348125c26a9bfe9c38f56bf0f9f10b79c590d9c25c1825731f5043644cdf7"
     end
   end
 
