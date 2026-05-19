74563366d65e248e0abf483c690688221d00177a
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2026-05-19 11:59:03 +0000

    fix(NDX-729): increase upper bound for NOVA Python SDK in the Python app template
    -----------------
    
    update cli formula commit dd2fe2a2 / version 0.0.231

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.230"
+  version "0.0.231"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.230/novacli_macos_amd64-0.0.230.tar.gz"
-      sha256 "8bd12d2bcbe9c2bad4fa627d962ddf549696256c3121655587d0bd22520d3a8f"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.231/novacli_macos_amd64-0.0.231.tar.gz"
+      sha256 "eb59aea844e00076b3be778f5856f3a22b805a483bb30544799e6d297be00947"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.230/novacli_macos_arm64-0.0.230.tar.gz"
-      sha256 "bdb1bed74547d5c9a707844df84da3170d1d863aa676571f74f05a225f6092fe"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.231/novacli_macos_arm64-0.0.231.tar.gz"
+      sha256 "f9b53716b3c8a2bc1a109d777798e93324bc2ecb0c51323a4b7af262084feff9"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.230/novacli_linux_amd64-0.0.230.tar.gz"
-      sha256 "01fdd4dc36aa6105025eafc7bb88f3f7de8aa866bc989b38c13f7aff6aa61259"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.231/novacli_linux_amd64-0.0.231.tar.gz"
+      sha256 "a470b74d50f5172824d0f8ea44929852d602175dba8633d14d6056a8fb837757"
     end
   end
 
