077508ad5b74fe1be040cb209b2dd8b8c07f388c
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2026-06-17 05:29:25 +0000

    Fix instance readiness polling recovery
    -----------------
    
    update cli formula commit 79df66eb / version 0.0.233

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,23 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.232"
+  version "0.0.233"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.232/novacli_macos_amd64-0.0.232.tar.gz"
-      sha256 "75005d494444c62fffe1d3ad9707f6c6d5c6a89ec1d0e86c3b9988140b43c80c"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.233/novacli_macos_amd64-0.0.233.tar.gz"
+      sha256 "b7cd7aace386a196b634e4a6e001efc52f363b31bbf02ae6d42631917e800c6f"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.232/novacli_macos_arm64-0.0.232.tar.gz"
-      sha256 "b51d387c75954ff7267204d085bb5e3decd82917c7c2d8ff9f7a1c649751aa1c"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.233/novacli_macos_arm64-0.0.233.tar.gz"
+      sha256 "004d8984dbf31cdfa2c6f946747d38b8bcf2a50b533271a1e8d2e17ca5b9a137"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.232/novacli_linux_amd64-0.0.232.tar.gz"
-      sha256 "140fb04fdbdf5a0a16bb2a0d83aa9b13658ab2b767bf2a3c5c525b74e542f682"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.233/novacli_linux_amd64-0.0.233.tar.gz"
+      sha256 "a64ef50c5d88ed9c63a6596859d37203b7e0bc9de9dd35cf481a7f124c4cab1b"
     end
   end
 
