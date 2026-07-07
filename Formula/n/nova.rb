72bd01528eb383f2e4aacba88270ffe0893fd8cd
uwe_from_woscli <uwe_from_woscli@wandelbots.com>
2026-07-07 14:43:39 +0000

    feat: linux arm64 support
    -----------------
    
    update cli formula commit 0562669d / version 0.0.235

--- Formula/n/nova.rb
+++ Formula/n/nova.rb
@@ -1,23 +1,27 @@
 class Nova < Formula
   desc "wandelbots cli to interact with wandelbots platform"
   homepage "https://github.com/wandelbotsgmbh/nova-cli"
-  version "0.0.234"
+  version "0.0.235"
 
   on_macos do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.234/novacli_macos_amd64-0.0.234.tar.gz"
-      sha256 "932c3488104fa5d43b484caf029f2aa50c3d3ec6362174cf4997ef418d146d17"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.235/novacli_macos_amd64-0.0.235.tar.gz"
+      sha256 "7cf01669b9b38bb12ca5a72e3a541a0cf74ed9bc73f4e7148444709961504889"
     end
     on_arm do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.234/novacli_macos_arm64-0.0.234.tar.gz"
-      sha256 "96b9713ff84b94397185f0d28825628d8f4be0ea9b2e737a629e18d6317375ed"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.235/novacli_macos_arm64-0.0.235.tar.gz"
+      sha256 "0409ca5c085a864236dcbf5b6c433809083588efb4f1d7859e0379c4685d3c70"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.234/novacli_linux_amd64-0.0.234.tar.gz"
-      sha256 "2408273ecec1a448f7edb3de38b98eca38faa8f8c12cda81bc1f1a9ee3c801c2"
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.235/novacli_linux_amd64-0.0.235.tar.gz"
+      sha256 "533cb63295dbb0a28bea1ed6babb58c7427cc709a252e1c8bbe29a58b62bf251"
+    end
+    on_arm do
+      url "https://github.com/wandelbotsgmbh/nova-cli/releases/download/0.0.235/novacli_linux_arm64-0.0.235.tar.gz"
+      sha256 "3e2d2a568cdf3d0ee46196d8bc21a3c623c46cfedd10ae36f318ce2cf335bf81"
     end
   end
 
